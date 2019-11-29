0dc7763ab0b42eed174e783a666a6d9d0c975df9
Óscar Hernández <oscarhernandezgomez@gmail.com>
2019-11-29 11:15:33 +0100

    Positions and laps

--- Unity Delivery/Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
+++ Unity Delivery/Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
@@ -9,13 +9,31 @@ namespace UnityStandardAssets.Vehicles.Car
     {
         private CarController m_Car; // the car controller we want to use
 
+        private float lastPosTime = 0f;
+
+        private int currentLap = 0;
+        private float lapStart = 0f;
+        private bool halfLap = false;
 
         private void Awake()
         {
             // get the car controller
             m_Car = GetComponent<CarController>();
+
+            lapStart = Time.time;
         }
 
+        private void Update()
+        {
+            if (Time.timeSinceLevelLoad - lastPosTime > 0.1f)
+            {
+                float timeStamp = Time.time;
+                Vector3 position = transform.position;
+                Vector3 velocity = GetComponent<Rigidbody>().velocity;
+                Quaternion rotation = transform.rotation;
+            }
+
+        }
 
         private void FixedUpdate()
         {
@@ -28,6 +46,30 @@ namespace UnityStandardAssets.Vehicles.Car
 #else
             m_Car.Move(h, v, v, 0f);
 #endif
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Finish"))
+            {
+                CheckLapFinish();
+            }
+            else if (other.CompareTag("Check"))
+            {
+                halfLap = true;
+            }
+        }
+
+        private void CheckLapFinish()
+        {
+            // Collide with half lap collider first, avoid multiple laps while being still
+            if (halfLap)
+            {
+                halfLap = false;
+                uint id = 0u;
+                float time = Time.time - lapStart;
+                lapStart = Time.time;
+            }
         }
     }
 }
